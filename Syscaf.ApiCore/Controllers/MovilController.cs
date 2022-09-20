b58ad80e2910d0d12c8f6e3a0ef7d458902f054d
Syscaf <66860235+Syscaf@users.noreply.github.com>
2022-09-19 20:02:57 -0500

    2022-09-19

--- Syscaf.ApiCore/Controllers/MovilController.cs
+++ Syscaf.ApiCore/Controllers/MovilController.cs
@@ -57,45 +57,80 @@ namespace Syscaf.ApiCore.Controllers
             return await _Movil.GetPreguntasPreoperacional(UsuarioId, NombrePlantilla, TipoPregunta, ClienteId);
         }
 
-        [HttpGet("BlobService")]
-        public async Task<string> BlobService()
+        [HttpPost("BlobService")]
+        public async Task<string> BlobService([FromForm] FileDataDTO datos)
         {
 
             var connectionString = "DefaultEndpointsProtocol=https;AccountName=neptunodataaccount;AccountKey=ONCwJMNPn4N9a960rBu8ontFlcQTbiGtK2inKFQo80BUAgO7n75n97B07rNhCeU6Wc8Coi5kozj4+AStIFkGkw==;EndpointSuffix=core.windows.net";
-            string containerName = "serviciotecnico";
-            var serviceClient = new BlobServiceClient(connectionString);
-            var containerClient = serviceClient.GetBlobContainerClient(containerName);
 
-            var fileName = "Documentos/1/2/3/Testfile.txt";
+            var serviceClient = new BlobServiceClient(connectionString);
+            var containerClient = serviceClient.GetBlobContainerClient(datos.contenedor);
+
+            var fileName = $"{datos.src}/{datos.archivo.FileName}";
 
             var blobClient = containerClient.GetBlobClient(fileName);
 
+            // comvertimos los datos en imemory stream
+            if (datos.archivo.Length > 0)
+            {
+                using (var ms = new MemoryStream())
+                {
+                    datos.archivo.CopyTo(ms);
+                    //   var fileBytes = ms.ToArray();
+                    // string s = Convert.ToBase64String(fileBytes);
+                    //  await blobClient.DeleteAsync();
+                    ms.Position = 0;
+                    if (!blobClient.Exists())
+                        await blobClient.UploadAsync(ms);
+                }
+               
 
-            Console.WriteLine("Uploading to Blob storage");
-            BinaryData data = new BinaryData("como lo mencione ahorita");
-          //  await blobClient.DeleteAsync();
-            if(!blobClient.Exists())
-            await blobClient.UploadAsync(data);
-
-
-         
+            }
             return blobClient.Uri.AbsolutePath;
         }
         [HttpGet("DownloadFileFromBlob")]
-        public async Task<MemoryStream> DownloadFileFromBlob()
+        public async Task<MemoryStream> DownloadFileFromBlob(string nombrearchivo)
         {
 
             var connectionString = "DefaultEndpointsProtocol=https;AccountName=neptunodataaccount;AccountKey=ONCwJMNPn4N9a960rBu8ontFlcQTbiGtK2inKFQo80BUAgO7n75n97B07rNhCeU6Wc8Coi5kozj4+AStIFkGkw==;EndpointSuffix=core.windows.net";
-            string containerName = "serviciotecnico";
+
             var serviceClient = new BlobServiceClient(connectionString);
-            var containerClient = serviceClient.GetBlobContainerClient(containerName);
-            var fileName = "Documentos/1/2/3/Testfile.txt";
-            var blobClient = containerClient.GetBlobClient(fileName);  
+            var containerClient = serviceClient.GetBlobContainerClient("serviciotecnico");
+            var blobClient = containerClient.GetBlobClient(nombrearchivo);
             MemoryStream stream = new MemoryStream();
             await blobClient.DownloadToAsync(stream);
             stream.Position = 0;
             return stream;
         }
+
+        [HttpGet("getDirectorio")]
+        public async Task<object> getDirectorio(string container)
+        {
+
+            var connectionString = "DefaultEndpointsProtocol=https;AccountName=neptunodataaccount;AccountKey=ONCwJMNPn4N9a960rBu8ontFlcQTbiGtK2inKFQo80BUAgO7n75n97B07rNhCeU6Wc8Coi5kozj4+AStIFkGkw==;EndpointSuffix=core.windows.net";
+
+            var serviceClient = new BlobServiceClient(connectionString);
+            var containerClient = serviceClient.GetBlobContainerClient("serviciotecnico");
+            
+           
+
+            return containerClient.GetBlobs().ToList().Select(s => s.Name);
+        }
+
+
+
+    }
+
+    public class FileDataDTO
+    {
+        public FileDataDTO()
+        {
+            contenedor = "serviciotecnico";
+        }
+        public string? nombre { get; set; }
+        public string? contenedor { get; set; }
+        public string? src { get; set; }
+        public IFormFile? archivo { get; set; }
 
     }
 }
