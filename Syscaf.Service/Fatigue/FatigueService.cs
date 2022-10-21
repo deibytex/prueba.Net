49f0d1be47c75b499dc8704f8a6f94c00d465544
Syscaf <66860235+Syscaf@users.noreply.github.com>
2022-10-21 08:32:07 -0500

    2022-10-21

---
+++ Syscaf.Service/Fatigue/FatigueService.cs
@@ -0,0 +1,34 @@
+﻿using Syscaf.Common.Integrate.LogNotificaciones;
+using Syscaf.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Syscaf.Service.Fatigue
+{
+    internal class FatigueService : IFatigueService
+    {
+        private readonly SyscafCoreConn _connCore;
+        private readonly ILogService _log;
+        private readonly ISyscafConn _conProd;
+        public FatigueService(SyscafCoreConn conn, ILogService _log, ISyscafConn _conProd)
+        {
+            _connCore = conn;
+            this._log = _log;
+            this._conProd = _conProd;
+        }
+
+
+        //public async  Task<List<dynamic>> GetFatigue(string Periodo, int ClienteIds) { 
+
+
+        
+        //}
+    }
+
+    internal interface IFatigueService
+    {
+    }
+}
