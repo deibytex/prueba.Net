8c73f5849e838cf892ec83d6b425a75bb7443f57
MarcialDiaz <marcial.diaz@syscaf.co>
2023-01-17 15:24:38 -0500

    Cambios

--- Syscaf.ApiCore/Utilidades/InterfacesAplication.cs
+++ Syscaf.ApiCore/Utilidades/InterfacesAplication.cs
@@ -3,6 +3,7 @@ using Syscaf.Common.Integrate.LogNotificaciones;
 using Syscaf.Service.Auth;
 using Syscaf.Service.Drive;
 using Syscaf.Service.eBus.Gcp;
+using Syscaf.Service.Fatigue;
 using Syscaf.Service.Peg;
 using Syscaf.Service.Portal;
 using SyscafWebApi.Service;
@@ -23,6 +24,7 @@ namespace Syscaf.ApiCore.Utilidades
             services.AddTransient<IMovilService, MovilService>();
             services.AddTransient<IArchivosService, ArchivosService>();
             services.AddTransient<IAdmService, AdmService>();
+            services.AddTransient<IFatigueService, FatigueService>();
         }
     }
 }
